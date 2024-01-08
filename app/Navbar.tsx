30aac2435d76d0c45f45a812b59399970405f46e
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-01-08 16:04:48 +0100

    completed mobile nav section

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,43 +1,86 @@
+'use client'
+
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
+import { IoMenuSharp } from 'react-icons/io5'
+import { AiOutlineClose } from 'react-icons/ai'
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <nav className="w-full h-20 flex items-center justify-between px-6 md:px-12 bg-blue-100 drop-shadow-lg shadow-xl">
-      <Link href="/">
-        <img src="/img/logo-1.png" className="h-16" alt="logo" />
-      </Link>
+    <>
+      <nav className="w-full h-20 flex items-center justify-between px-6 md:px-12 bg-blue-100 drop-shadow-lg shadow-xl relative">
+        <Link href="/">
+          <img src="/img/logo-1.png" className="h-16" alt="logo" />
+        </Link>
 
-      <ul className="flex items-center space-x-8 text-blue-950 text-md font-light">
-        <Link
-          href="/"
-          className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
-        >
-          Home
-        </Link>
-        <Link
-          href="/"
-          className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
-        >
-          Featured
-        </Link>
-        <Link
-          href="/"
-          className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
-        >
-          Agents
-        </Link>
-        <Link
-          href="/"
-          className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
-        >
-          News
-        </Link>
-        <Link href="/" className="bg-blue-900 text-white px-4 py-2">
-          Login
-        </Link>
-      </ul>
-    </nav>
+        <ul className="hidden md:flex items-center space-x-8 text-blue-950 text-md font-light">
+          <Link
+            href="/"
+            className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
+          >
+            Home
+          </Link>
+          <Link
+            href="/"
+            className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
+          >
+            Featured
+          </Link>
+          <Link
+            href="/"
+            className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
+          >
+            Agents
+          </Link>
+          <Link
+            href="/"
+            className="hover:font-semibold hover:border-b-2 px-2 hover:py-4 border-blue-950"
+          >
+            News
+          </Link>
+          <Link href="/" className="bg-blue-900 text-white px-4 py-2">
+            Login
+          </Link>
+        </ul>
+
+        {open ? (
+          <AiOutlineClose
+            onClick={() => setOpen(!open)}
+            className="md:hidden text-4xl font-bold text-blue-900"
+          />
+        ) : (
+          <IoMenuSharp
+            onClick={() => setOpen(!open)}
+            className="md:hidden text-4xl font-bold text-blue-900"
+          />
+        )}
+      </nav>
+
+      {open && (
+        <nav className="w-full flex flex-col px-10 py-2 space-y-4 absolute bg-blue-100">
+          <Link onClick={() => setOpen(!open)} href="/" className="">
+            Home
+          </Link>
+          <Link onClick={() => setOpen(!open)} href="/featured" className="">
+            Featured
+          </Link>
+          <Link onClick={() => setOpen(!open)} href="/agents" className="">
+            Agents
+          </Link>
+          <Link onClick={() => setOpen(!open)} href="/news" className="">
+            News
+          </Link>
+          <Link
+            onClick={() => setOpen(!open)}
+            href="/login"
+            className="bg-blue-900 text-white px-4 py-2"
+          >
+            Login
+          </Link>
+        </nav>
+      )}
+    </>
   )
 }
 
